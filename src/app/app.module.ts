93c96761c6976a0224ba2b56fdcc3552a987698f
Ginelle-19 <101726053+Ginelle-19@users.noreply.github.com>
2023-02-28 13:13:44 +0800

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-// import { HelloComponent } from './hello.component';
-
 import { MyServiceService } from './my-service.service';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  // declarations: [ AppComponent, HelloComponent ],
   bootstrap: [AppComponent],
   declarations: [AppComponent, NewCmpComponent],
   providers: [MyServiceService],
