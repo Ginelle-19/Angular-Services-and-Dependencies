3f35b8ba5f1f52cfc892cf490a66d4af1967ca7e
Ginelle-19 <101726053+Ginelle-19@users.noreply.github.com>
2023-02-28 22:45:13 +0800

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MyServiceService } from './my-service.service';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { HttpclientComponent } from './httpclient/httpclient.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpclientService } from './httpclient.service';
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent],
-  declarations: [AppComponent, NewCmpComponent],
-  providers: [MyServiceService],
+  declarations: [AppComponent, NewCmpComponent, HttpclientComponent],
+  providers: [MyServiceService, HttpclientService],
 })
 export class AppModule {}
